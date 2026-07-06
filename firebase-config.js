e3a8703d1f0e2e716a65d267c4ed8b13e0bdd4a2
sofiiaappopovvych <skillfulsewing@gmail.com>
2026-07-06 14:43:48 -0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,8 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCc_fFOwZUgKmhD1L7fVcm8lnDTcykX2hU",
-    authDomain: "ompany-sales-final.firebaseapp.com",
-    projectId: "ompany-sales-final",
-    storageBucket: "ompany-sales-final.firebasestorage.app",
-    messagingSenderId: "121480749634",
-    appId: "1:121480749634:web:a0a2d6c36c9bf6be4303db"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+export const firebaseConfig = {
+  apiKey: "AIzaSyCc_fFQwZUqKmhD1L7fVcm81nDTcykX2hU",
+  authDomain: "ompany-sales-final.firebaseapp.com",
+  projectId: "ompany-sales-final",
+  storageBucket: "ompany-sales-final.firebasestorage.app",
+  messagingSenderId: "121480749634",
+  appId: "1:121480749634:web:a0a2d6c36c9bf6be4303db"
+};
